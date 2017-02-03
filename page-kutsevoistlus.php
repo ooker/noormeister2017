c5c5826ca1da1139e41f1a29eed4263eeb170f83
Oliver Maaker <maaker@gmail.com>
2017-02-03 23:17:17 +0200

    Kutsevõisttlus - test

--- page-kutsevoistlus.php
+++ page-kutsevoistlus.php
@@ -5,26 +5,6 @@ Template Post Type: page, kutsevoistlus
 */
 ?>
 
-<?php
-    /*$loop = new WP_Query( array( 'post_type' => 'tootuba', 'ignore_sticky_posts' => 1 ) );
-    if ( $loop->have_posts() ) :
-        while ( $loop->have_posts() ) : $loop->the_post(); ?>
-            <div class="pindex">
-                <?php if ( has_post_thumbnail() ) { ?>
-                    <div class="pimage">
-                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-                    </div>
-                <?php } ?>
-                <div class="ptitle">
-                    <h2><?php echo get_the_title(); ?></h2>
-                </div>
-            </div>
-        <?php endwhile;
-    endif;
-    wp_reset_postdata();*/
-?>
-
-
 <?php get_header() ?>
 
 		<div class="container nm-pageContent">
@@ -59,19 +39,7 @@ Template Post Type: page, kutsevoistlus
             ?>
 
             <!-- End Loop -->
-
-          <!--<?php
-						$args = array( 'post_type' => 'tootuba' );
-						$recent_posts = wp_get_recent_posts( $args );
-						foreach( $recent_posts as $recent ){
-							echo '<a href="#" data-id="' . $recent["ID"] . '" data-modal-type="tootuba" class="nm-modal-opener">';
-                echo '<img src="' .  $recent["post_thumbnail"] . '">';
-                echo '<p class="">' . $recent["post_title"] . '</p>';
-              echo '</a>';
-						}
-						wp_reset_query();
-					?>-->
-					</div>
+				  </div>
 				</div>
 
 			</section>
